3d307c67356dc259eba65a392eeda0853c34c978
israelias <61286823+israelias@users.noreply.github.com>
2022-12-12 16:29:00 +0800

    ci: rename aws env vars for vercel

--- core/settings.py
+++ core/settings.py
@@ -185,8 +185,8 @@ if "USE_AWS" in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = "thrifthub"
     AWS_S3_REGION_NAME = "ap-southeast-1"
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_ACCESS_KEY_ID = os.environ.get("ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("SECRET_ACCESS_KEY")
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
     # Static and media files
