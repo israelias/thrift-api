eaa1864a6415d0c79deb7baf6030550e0846f096
israelias <61286823+israelias@users.noreply.github.com>
2022-12-12 16:58:57 +0800

    chore: add comments for wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+# should run the  app for vercel
 application = get_wsgi_application()
