6c9833ced79950a3b91c20473328da23606c87ac
Joem Elias Sanez <israelias.js@gmail.com>
2022-12-28 03:59:24 +0800

    Adjust admin views for order, store and vendor

--- order/admin.py
+++ order/admin.py
@@ -12,6 +12,14 @@ class OrderDetailInline(admin.StackedInline):
 
 @admin.register(OrderModel)
 class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'vendor', 'buyer', 'status')
+    # list_fiter = ['product', 'status']
+    search_fields = ('vendor', 'buyer', 'product')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
     inlines = [
         OrderDetailInline,
     ]
--- store/admin.py
+++ store/admin.py
@@ -1,19 +1,48 @@
 from django.contrib import admin
-from mptt.admin import MPTTModelAdmin
-
+from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
+from versatileimagefield.fields import SizedImageCenterpointClickDjangoAdminField
 
 from .models import Category, Image, Product
 
-admin.site.register(Category, MPTTModelAdmin)
+admin.site.register(Category, DraggableMPTTAdmin)
 admin.site.register(Image)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'owner', 'title']
 
 
 class ImageInline(admin.StackedInline):
     model = Image
+    fk_name = "product"
+    # image = SizedImageCenterpointClickDjangoAdminField(required=False)
+    fields = ("name", "alt_text", "is_feature", "product",  "id")
+
+# list_display = ('__str__',)
+#     list_display_links = ()
+#     list_filter = ()
+#     list_select_related = False
+#     list_per_page = 100
+#     list_max_show_all = 200
+#     list_editable = ()
+#     search_fields = ()
+#     date_hierarchy = None
+#     save_as = False
+#     save_as_continue = True
+#     save_on_top = False
+#     paginator = Paginator
+#     preserve_filters = True
+#     inlines = []
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    # list_display_links = ['id', 'vendor', 'category']
+    list_select_related = True
+    list_display = ['id', 'title', 'vendor', 'is_available', 'condition', 'category', 'created_at']
+    list_fiter = ('category', 'is_available', 'vendor' 'condition')
+    search_fields = ['title', 'vendor', 'description']
+    prepopulated_fields = {'slug': ('title',)}
     inlines = [
         ImageInline,
       
