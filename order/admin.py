d8c1d7ae63d613df9ad70a66e14b0213b8b30ee6
Joem Elias Sanez <israelias.js@gmail.com>
2022-12-29 14:07:41 +0800

    Generate dec docs and format Order app

--- order/admin.py
+++ order/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from store.models import Product
-from vendor.models import Vendor
 
 from .models import Order as OrderModel
 from .models import OrderDetail
@@ -12,9 +10,8 @@ class OrderDetailInline(admin.StackedInline):
 
 @admin.register(OrderModel)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'product', 'vendor', 'buyer', 'status')
-    # list_fiter = ['product', 'status']
-    search_fields = ('vendor', 'buyer', 'product')
+    list_display = ("id", "product", "vendor", "buyer", "status")
+    search_fields = ("vendor", "buyer", "product")
 
     filter_horizontal = ()
     list_filter = ()
